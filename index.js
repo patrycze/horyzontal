d8b646ebee5ad99d44db6c2e101d9866e452696e
patrycze <patryk.pazura@gmail.com>
2017-07-02 14:15:59 +0200

    new

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-var express = require("express");
+var express = require('express');
 var app = express();
 
-app.set('port', (process.env.PORT || 8000));
 
-app.use(express.static(__dirname + '/views'));
+app.use(express.static(__dirname + '/public'));
+
+app.set('port', (process.env.PORT || 8000));
 
 /*
 //views is directory for all template files 
@@ -15,4 +16,4 @@ app.get("/", function(request, response) {
 });
 */
 
-app.listen(8000);
+app.listen(port);
